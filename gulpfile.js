de3ce315309aeaea0b5f66932a7add85cc3ab9b4
Francois-Xavier Gentilhomme <gentilhomme.fx@outlook.fr>
2015-05-26 20:41:12 +0200

    Amend gulpfile to allow separate base and css build

--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,21 @@ var gulp = require('gulp-param')(require('gulp'), process.argv),
     pkg = require('./package.json');
     
     
-//gulp.task('base', function() {
-//  return gulp.src(['src/hand.base.js'])
-//    .pipe(rename({suffix: '.min'}))
-//    .pipe(uglify())
-//    .pipe(gulp.dest('dist/'));
-//});
-//
-//gulp.task('css', function() {
-//  return gulp.src(['src/hand.css.js'])
-//    .pipe(rename({suffix: '.min'}))
-//    .pipe(uglify())
-//    .pipe(gulp.dest('dist/'));
-//});
-
-gulp.task('concat-default', function() {
-    return gulp.src([
-        'src/hand.base.js',
-        'src/hand.css.js'
-    ])
-    .pipe(concat('hand.js'))
-    .pipe(gulp.dest('bin/'))
+gulp.task('base', function() {
+  return gulp.src(['src/hand.base.js'])
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest('bin/'));
 });
+
+gulp.task('css', function() {
+  return gulp.src(['src/hand.css.js'])
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('bin/'));
+});
+
+gulp.task('concat-default', ['base', 'css']);
 
 gulp.task('concat-versioned', function() {
     return gulp.src([
